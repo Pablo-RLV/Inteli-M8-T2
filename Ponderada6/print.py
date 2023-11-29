874b36df264b38ee1fc8212adf5c42a7ea4ac207
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2023-11-29 10:56:35 -0300

    refactor: :recycle: update output file

--- Ponderada6/print.py
+++ Ponderada6/print.py
@@ -3,32 +3,23 @@ from perceptron import Gate
 
 gate = Gate()
 
-test1 = np.array([0, 0])
-test2 = np.array([1, 0])
-test3 = np.array([0, 1])
-test4 = np.array([1, 1])
+inputs = [
+    (0, 0),
+    (1, 0),
+    (0, 1),
+    (1, 1)
+]
 
-print("AND({}, {}) = {}".format(0, 0, gate.AND_Function(test1)))
-print("AND({}, {}) = {}".format(1, 0, gate.AND_Function(test2)))
-print("AND({}, {}) = {}".format(0, 1, gate.AND_Function(test3)))
-print("AND({}, {}) = {} \n".format(1, 1, gate.AND_Function(test4)))
+logic_gates = {
+    'AND': gate.AND_Function,
+    'OR': gate.OR_Function,
+    'NAND': gate.NAND_Function,
+    'NOR': gate.NOR_Function,
+    'XOR': gate.XOR_Function
+}
 
-print("OR({}, {}) = {}".format(0, 0, gate.OR_Function(test1)))
-print("OR({}, {}) = {}".format(1, 0, gate.OR_Function(test2)))
-print("OR({}, {}) = {}".format(0, 1, gate.OR_Function(test3)))
-print("OR({}, {}) = {} \n".format(1, 1, gate.OR_Function(test4)))
-
-print("NAND({}, {}) = {}".format(0, 0, gate.NAND_Function(test1)))
-print("NAND({}, {}) = {}".format(1, 0, gate.NAND_Function(test2)))
-print("NAND({}, {}) = {}".format(0, 1, gate.NAND_Function(test3)))
-print("NAND({}, {}) = {} \n".format(1, 1, gate.NAND_Function(test4)))
-
-print("NOR({}, {}) = {}".format(0, 0, gate.NOR_Function(test1)))
-print("NOR({}, {}) = {}".format(1, 0, gate.NOR_Function(test2)))
-print("NOR({}, {}) = {}".format(0, 1, gate.NOR_Function(test3)))
-print("NOR({}, {}) = {} \n".format(1, 1, gate.NOR_Function(test4)))
-
-print("XOR({}, {}) = {}".format(0, 0, gate.XOR_Function(test1)))
-print("XOR({}, {}) = {}".format(1, 0, gate.XOR_Function(test2)))
-print("XOR({}, {}) = {}".format(0, 1, gate.XOR_Function(test3)))
-print("XOR({}, {}) = {} \n".format(1, 1, gate.XOR_Function(test4)))
+for gate_name, gate_function in logic_gates.items():
+    print(f"\nTesting {gate_name} gate:")
+    for input_values in inputs:
+        result = gate_function(np.array(input_values))
+        print(f"{gate_name}({input_values[0]}, {input_values[1]}) = {result}")
